4bd4c9700d57a1366c8f960cf135f7d7f0aa1225
lantimilan <lantimilan@gmail.com>
2026-02-20 15:14:47 -0800

    threadpool with worker loop

---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+// src/lib.rs
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+pub struct ThreadPool{
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+// struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+// 1. When spawn a thread, it runs the worker code, each worker has id
+// 2. the worker code runs a loop on polling the request queue
+// 3. the request is processed by invoking the closure
+//
+// use Worker to send closure to thread for execution
+// each worker holds its JoinHandle
+// therefore the threadpool is holding a vector of workers
+//
+// internally, threadpool uses a queue to manage requests
+// and in rust we use channel for thread-safe queue
+// one-producer-many-consumers
+// 1. threadpool creates a channel and hold on to the sender
+// 2. worker hold on to the receiver
+// 3. struct Job to hold the closure sent down the channel
+// 4. the execute method will send the job through the sender
+// 5. in worker thread, worker will loop over its receiver and
+// executes the closure
+impl ThreadPool {
+    /// Creates a new ThreadPool.
+    ///
+    /// The size is the number of threads in that pool.
+    ///
+    /// # Panics
+    /// The `new` function will panic if size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        // channel is multi-producer, single-consumer
+        let (sender, receiver) = mpsc::channel();
+
+        // wrap around receiver with mutex and shared_ptr
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            // create some threads and store them in the vector
+            // receiver is a shared_ptr and guarded by a mutex
+            // since it can be accessed by multiple threads concurrently
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
+    }
+
+    // execute takes a closure f as its arg
+    pub fn execute<F>(&self, f: F)
+        where
+            // FnOnce means a closure that takes void and returns void
+            F: FnOnce() + Send + 'static,
+    {
+        // execute just enqueues f and some worker will poll the queue
+        // to actually run the closure
+        let job = Box::new(f);
+
+        // unwrap means throw if error
+        self.sender.send(job).unwrap();
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // spawn a thread for this worker
+        // the returned thread is a JoinHandle
+        // TODO(lyan): use std::thread::Builder::spawn
+        // which returns Result, instead of panic (throw exception)
+        let thread = thread::spawn(move || {
+            // we need to run a loop to keep polling the queue
+            // and run job
+            loop {
+                // lock acquires mutex
+                // unwrap may panic, could use expect for error handling
+                // recv polls job from channel
+                // the recv call may block if channel is empty
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                // id is a member of Worker
+                println!("Worker {id} got a job, executing.");
+                // job is a closure that is callable
+                job();
+            }
+        });
+
+        Worker {id, thread}
+    }
+
+    // the worker thread should run a loop
+    // that poll queue and execute the closure
+    // also need a mechanism for terminate
+}
--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,17 @@ use std::{
     time::Duration,
 };
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        // spawn one thread per request
-        thread::spawn(|| {
+        // thread pool execution
+        pool.execute(|| {
             handle_connection(stream);
         });
 
